c844ae0f1af0835e9621a8b8775a399a06dcdc76
Nick Warren <nickwarren47@gmail.com>
2022-08-25 12:15:36 -0600

    Added new route paths, css work, and clickable pictures

--- lightsaber_app_front-end/src/components/NavBar.js
+++ lightsaber_app_front-end/src/components/NavBar.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { Menu, Icon } from 'semantic-ui-react';
+import {Link} from 'react-router-dom';
 
 function NavBar() {
     return(
-        <header>
-            <h1>Lightsaber Destiny</h1>
-        </header>
+    <Menu className='menu'>
+      <Menu.Item>
+        <Link to="/">
+          <Icon name="home" />
+        </Link>
+      </Menu.Item>
+      <Menu.Item>
+        <Link to="/users">
+          <Icon name="users" />
+        </Link>
+      </Menu.Item>
+    </Menu>
     )
 }
 
