627314d0f83a5c52a825200b3dc234f486b577f5
Nick Warren <nickwarren47@gmail.com>
2022-08-25 18:21:51 -0600

    Added css work

--- lightsaber_app_front-end/src/components/Homepage.js
+++ lightsaber_app_front-end/src/components/Homepage.js
@@ -38,7 +38,7 @@ function Homepage(){
     return(
         <div className='homePage'>
             <h1 id="destiny"> CHOOSE YOUR DESTINY </h1>
-            <h3 id="objective">Choose whether to follow the path of the Jedi or the Sith.</h3>
+            <h3 id="objective">Choose whether to follow the path of the Jedi or the Sith...</h3>
             <img id="jedi_img" onClick={() => handleClick(jedi)} src="https://user-images.githubusercontent.com/106715328/186465898-8ac00d99-597c-45a2-b53b-0f8c5ff86d15.jpg" alt="light side"/>
             <img id="sith_img" onClick={() => handleClicked(sith)} src="https://user-images.githubusercontent.com/106715328/186466229-eb665631-46e8-4479-a5e8-fef783fa675d.jpg" alt="dark side"/>
         </div>
